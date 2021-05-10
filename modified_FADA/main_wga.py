414a74f295c110f9168098b31f1214418e2ffd35
Bloodline-2000 <1065673488@qq.com>
2021-05-10 10:56:23 +0800

    wga version

---
+++ modified_FADA/main_wga.py
@@ -0,0 +1,124 @@
+import argparse
+import torch
+import dataloader
+from models import main_models
+import numpy as np
+
+parser=argparse.ArgumentParser()
+parser.add_argument('--n_epoches_1',type=int,default=10)
+parser.add_argument('--n_epoches_2',type=int,default=100)
+parser.add_argument('--n_epoches_3',type=int,default=100)
+parser.add_argument('--n_target_samples',type=int,default=7)
+parser.add_argument('--batch_size',type=int,default=64)
+
+opt=vars(parser.parse_args())
+
+use_cuda=True if torch.cuda.is_available() else False
+device=torch.device('cuda:0') if use_cuda else torch.device('cpu')
+torch.manual_seed(1)
+if use_cuda:
+    torch.cuda.manual_seed(1)
+train_dataloader=dataloader.mnist_dataloader(batch_size=opt['batch_size'],train=True)
+test_dataloader=dataloader.mnist_dataloader(batch_size=opt['batch_size'],train=False)
+
+classifier=main_models.Classifier()
+encoder=main_models.Encoder()
+discriminator=main_models.DCD(input_features=128)
+attention = main_models.Attention(input_features=64)
+# TODO: attention需要有初始化参数，感觉在0.5左右会好一点
+
+classifier.to(device)
+encoder.to(device)
+discriminator.to(device)
+attention.to(device)
+loss_fn=torch.nn.CrossEntropyLoss()
+X_s,Y_s=dataloader.sample_data()
+X_t,Y_t=dataloader.create_target_samples(opt['n_target_samples'])
+#-------------------training for step 3-------------------
+optimizer_all=torch.optim.Adam(list(encoder.parameters())+list(classifier.parameters())+list(attention.parameters())+list(discriminator.parameters()),lr=0.001)
+test_dataloader=dataloader.svhn_dataloader(train=False,batch_size=opt['batch_size'])
+
+
+for epoch in range(opt['n_epoches_3']):
+    #---training g and h , DCD is frozen
+
+    groups, groups_y = dataloader.sample_groups(X_s,Y_s,X_t,Y_t,seed=opt['n_epoches_2']+epoch)
+    G1, G2, G3, G4 = groups
+    Y1, Y2, Y3, Y4 = groups_y
+    groups_2 = [G2, G4]
+    groups_y_2 = [Y2, Y4]
+
+    n_iters = 2 * len(G2)
+    index_list = torch.randperm(n_iters)
+
+    n_iters_dcd = 4 * len(G2)
+    index_list_dcd = torch.randperm(n_iters_dcd)
+
+    mini_batch_size_g_h = 20 #data only contains G2 and G4 ,so decrease mini_batch
+    mini_batch_size_dcd= 40 #data contains G1,G2,G3,G4 so use 40 as mini_batch
+    X1 = []
+    X2 = []
+    ground_truths_y1 = []
+    ground_truths_y2 = []
+    dcd_labels=[]
+    for index in range(n_iters):
+
+
+        ground_truth=index_list[index]//len(G2)
+        x1, x2 = groups_2[ground_truth][index_list[index] - len(G2) * ground_truth]
+        y1, y2 = groups_y_2[ground_truth][index_list[index] - len(G2) * ground_truth]
+        # y1=torch.LongTensor([y1.item()])
+        # y2=torch.LongTensor([y2.item()])
+        dcd_label=0 if ground_truth==0 else 2
+        X1.append(x1)
+        X2.append(x2)
+        ground_truths_y1.append(y1)
+        ground_truths_y2.append(y2)
+        dcd_labels.append(dcd_label)
+
+        if (index+1)%mini_batch_size_g_h==0:
+
+            X1=torch.stack(X1)
+            X2=torch.stack(X2)
+            ground_truths_y1=torch.LongTensor(ground_truths_y1)
+            ground_truths_y2 = torch.LongTensor(ground_truths_y2)
+            dcd_labels=torch.LongTensor(dcd_labels)
+            X1=X1.to(device)
+            X2=X2.to(device)
+            ground_truths_y1=ground_truths_y1.to(device)
+            ground_truths_y2 = ground_truths_y2.to(device)
+            dcd_labels=dcd_labels.to(device)
+
+            optimizer_all.zero_grad()
+
+            encoder_X1=encoder(X1)
+            encoder_X2=encoder(X2)
+
+            attention_score1 = attention(encoder_X1)
+            attention_score2 = attention(encoder_X2)
+
+            attention_X1 = encoder_X1 * attention_score1
+            attention_X2 = encoder_X2 * attention_score2
+            X_cat = torch.cat([attention_X1, attention_X2], 1)
+            attention_X1_clf = encoder_X1 * (1-attention_score1)
+            attention_X2_clf = encoder_X2 * (1-attention_score2)
+            y_pred_X1 = classifier(attention_X1_clf)
+            y_pred_X2 = classifier(attention_X2_clf)
+            y_pred_dcd=discriminator(X_cat)
+
+            loss_X1=loss_fn(y_pred_X1,ground_truths_y1)
+            loss_X2=loss_fn(y_pred_X2,ground_truths_y2)
+            loss_dcd=loss_fn(y_pred_dcd,dcd_labels)
+
+            loss_sum = loss_X1 + loss_X2 + 0.2 * loss_dcd
+
+            loss_sum.backward()
+            optimizer_all.step()
+
+            X1 = []
+            X2 = []
+            ground_truths_y1 = []
+            ground_truths_y2 = []
+            dcd_labels = []
+
+
