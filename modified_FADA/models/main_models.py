2ae4ce63e33c8d005b8f3a3aef1e6d0d4ffd0045
Bloodline-2000 <1065673488@qq.com>
2021-05-26 18:45:45 +0800

    model upd

--- modified_FADA/models/main_models.py
+++ modified_FADA/models/main_models.py
@@ -1,11 +1,16 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
 from models.BasicModule import BasicModule
 from torchvision import models
 
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.shape[0], -1)
+
 class DCD(BasicModule):
-    def __init__(self,h_features=64,input_features=128):
+    def __init__(self,h_features=512,input_features=2048*2):
         super(DCD,self).__init__()
 
         self.fc1=nn.Linear(input_features,h_features)
@@ -19,7 +24,7 @@ class DCD(BasicModule):
 
 
 class Attention(BasicModule):
-    def __init__(self, h_features=128, input_features=64):
+    def __init__(self, h_features=512, input_features=2048):
         super(Attention, self).__init__()
 
         self.fc1 = nn.Linear(input_features, h_features)
@@ -34,71 +39,35 @@ class Attention(BasicModule):
         # return F.InstanceNorm1d(out)
 
 class Classifier(BasicModule):
-    def __init__(self,input_features=64):
+    def __init__(self,input_features=2048):
         super(Classifier,self).__init__()
-        self.fc=nn.Linear(input_features,10)
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(2048, 31),
+        )
 
     def forward(self,input):
-        return F.softmax(self.fc(input),dim=1)
+        return self.classifier(input)
 
-# class Encoder(BasicModule):
-#     def __init__(self):
-#         super(Encoder,self).__init__()
+class Encoder(nn.Module):
 
-#         self.conv1=nn.Conv2d(1,6,5)
-#         self.conv2=nn.Conv2d(6,16,5)
-#         self.fc1=nn.Linear(256,120)
-#         self.fc2=nn.Linear(120,84)
-#         self.fc3=nn.Linear(84,64)
-
-#     def forward(self,input):
-#         out=F.relu(self.conv1(input))
-#         out=F.max_pool2d(out,2)
-#         out=F.relu(self.conv2(out))
-#         out=F.max_pool2d(out,2)
-#         out=out.view(out.size(0),-1)
-
-#         out=F.relu(self.fc1(out))
-#         out=F.relu(self.fc2(out))
-#         out=self.fc3(out)
-
-#         return out
-
-
-class Encoder(BasicModule):
     def __init__(self):
-        super(Encoder,self).__init__()
+        super(Encoder, self).__init__()
+        resnet = torchvision.models.resnet50(pretrained=True) 
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
 
-        self.conv1=nn.Conv2d(1,6,5)
-        self.conv2=nn.Conv2d(6,16,5)
-        self.fc1=nn.Linear(256,120)
-        self.fc2=nn.Linear(120,84)
-        self.fc3=nn.Linear(84,64)
+        self.fine_tune()
 
-    def forward(self,input):
-        out=F.relu(self.conv1(input))
-        out=F.max_pool2d(out,2)
-        out=F.relu(self.conv2(out))
-        out=F.max_pool2d(out,2)
-        out=out.view(out.size(0),-1)
-
-        out=F.relu(self.fc1(out))
-        out=F.relu(self.fc2(out))
-        out=self.fc3(out)
-
+    def forward(self, images):
+        out = self.resnet(images)  # (batch_size, 2048, image_size/32, image_size/32)
+        out = out.view(out.size(0),-1)
         return out
 
-class ResNet18_Encoder(BasicModule):
-    def __init__(self):
-        super(ResNet18_Encoder, self).__init__()
-        self.resnet = models.resnet18(pretrained=False)
-        self.resnet.load_state_dict(torch.load('./models/resnet18-5c106cde.pth'))
-        for param in self.resnet.parameters():
-            param.requires_grad = False
-        num_ftrs = self.resnet.fc.in_features
-        self.resnet.fc = nn.Linear(num_ftrs, 64)
-
-    def forward(self, input):
-        # print(input.shape)
-        out = self.resnet(input)
-        return out
+    def fine_tune(self, fine_tune=True):
+        for p in self.resnet.parameters():
+            p.requires_grad = False
+        # If fine-tuning, only fine-tune convolutional blocks 2 through 4
+        for c in list(self.resnet.children())[5:]:
+            for p in c.parameters():
+                p.requires_grad = fine_tune 
